46696ef01dd984389a1d183ab5144cb60ab4c8a6
NgocHuy2207 <nngochuy2207@gmail.com>
2024-08-12 00:29:43 +0700

    add Chapter 8

---
+++ src/main/java/vn/hoidanit/laptopshop/sevice/UserService.java
@@ -0,0 +1,10 @@
+package vn.hoidanit.laptopshop.sevice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "Hello from service";
+    }
+}
